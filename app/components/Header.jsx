ba14ddc600bd3773d5810b91a2503761044d9b82
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-10-05 18:43:49 +0300

    "Added TextInput for search functionality to Header component"

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,22 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const Header = ({ props, navigation }) => {
   return (
-    <View className="mx-5">
+    <View className="mx-5 space-y-2">
       <View className="flex-row justify-between items-center">
         <View className="flex-row items-center space-x-2">
           <FontAwesome name="user-circle-o" size={35} color="black" />
           <View>
-            <Text>Welcome</Text>
-            <Text>{props.name}</Text>
+            <Text className="text-base">Welcome</Text>
+            <Text className="text-md font-bold">{props.name}</Text>
           </View>
         </View>
         <View>
           <Ionicons name="notifications" size={24} color="black" />
         </View>
+      </View>
+      <View className="bg-slate-300 flex-row p-2 rounded-xl space-x-2">
+        <Ionicons name="search" size={24} color="white" />
+        <TextInput placeholder="Search" />
       </View>
     </View>
   );
