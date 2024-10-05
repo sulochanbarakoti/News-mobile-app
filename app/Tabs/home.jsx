5f1f3dc58370318d2787b225bdb4951eb79647c0
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-10-05 23:04:55 +0300

    "Updated Home component to initialize allNews state with results, added handleTabClick function, and modified useEffect and TouchableOpacity onPress handlers."

--- app/Tabs/home.jsx
+++ app/Tabs/home.jsx
@@ -14,7 +14,7 @@ import axios from "axios";
 import { results } from "../../json";
 
 const Home = () => {
-  const [allNews, setAllNews] = useState([]);
+  const [allNews, setAllNews] = useState(results);
   const [activeTab, setActiveTab] = useState("All");
   let profileData = {
     name: "John Doe",
@@ -34,20 +34,37 @@ const Home = () => {
     "World",
   ];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          "https://newsdata.io/api/1/news?apikey=pub_55437e5d9bdac19426739db9c6786c467f2ae&language=en"
-        );
-        console.log(res.data.results);
-        setAllNews(res.data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    // fetchData();
-  }, [activeTab]);
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        try {
+          const res = await axios.get(
+            "https://newsdata.io/api/1/news?apikey=pub_55437e5d9bdac19426739db9c6786c467f2ae&language=en"
+          );
+          console.log(res.data.results);
+          setAllNews(res.data.results);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      // fetchData();
+    },
+    [activeTab],
+    [allNews]
+  );
+
+  const handleTabClick = (tab) => {
+    console.log(tab.toLowerCase());
+    if (tab !== "All") {
+      const updateNews = results.filter((data) =>
+        data.category.includes(tab.toLowerCase())
+      );
+      setAllNews(updateNews);
+    } else {
+      setAllNews(results);
+    }
+    setActiveTab(tab);
+  };
 
   return (
     <View className="mt-8 ">
@@ -61,7 +78,10 @@ const Home = () => {
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
             <View className="flex-row  gap-4">
               {tabs.map((tab, index) => (
-                <TouchableOpacity key={index} onPress={() => setActiveTab(tab)}>
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => handleTabClick(tab)}
+                >
                   <View
                     className={
                       activeTab === tab
@@ -87,7 +107,7 @@ const Home = () => {
           </ScrollView>
         </View>
         {/* Trending posts */}
-        <Trending props={results} />
+        <Trending props={allNews} />
       </View>
       <StatusBar style="auto" />
     </View>
